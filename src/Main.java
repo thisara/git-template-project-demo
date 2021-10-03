8280a59017162106669f23a184ce0a232704bf03
root <>
2021-10-03 15:10:33 +0530

    update main template in non-conflict way

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("This is an update from main stream!");
+        System.out.println("This is the main class");
+        System.out.println("This is non-conflict update from the main class");
     }
 }
